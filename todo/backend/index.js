0d7b6a4c3316782561befffbdc37ac4d438d2eea
POBOROJO <parijatbhatt1@gmail.com>
2024-09-29 15:06:08 +0530

    files re-updated

--- todo/backend/index.js
+++ todo/backend/index.js
@@ -1,4 +1,16 @@
-// write a basic express boilerplate code,
-// with express.json() middlware
-
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
