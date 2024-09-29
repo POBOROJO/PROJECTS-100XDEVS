3c2cabaf24fd6cb943ebeb920b47415153318453
POBOROJO <parijatbhatt1@gmail.com>
2024-09-29 10:44:45 +0530

    BE structure added

---
+++ todo/backend/config/db.js
@@ -0,0 +1,19 @@
+/*
+    *Todo{
+        title : string,
+        description : string,
+        cpmpleted : boolean
+    }
+*/
+
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+  } catch (error) {
+    console.error(error.message);
+  }
+};
