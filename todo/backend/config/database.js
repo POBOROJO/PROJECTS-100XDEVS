e9fc0859024bc698b1f5b161951e05fd1e08f268
POBOROJO <parijatbhatt1@gmail.com>
2024-10-03 23:55:52 +0530

    Added:Database Connection

--- todo/backend/config/database.js
+++ todo/backend/config/database.js
@@ -7,12 +7,11 @@ dotenv.config({
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Database connected successfully");
   } catch (error) {
     console.log(error);
   }
 };
+
+export default connectDB;
--- todo/backend/index.js
+++ todo/backend/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/database.js";
 
-dotenv.config();
+dotenv.config({
+  path: ".env",
+});
+
+connectDB();
 
 const app = express();
 
@@ -11,6 +16,6 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
