d4b2e2ccaa3f97b88b75f68fac3afd5fac46d674
POBOROJO <parijatbhatt1@gmail.com>
2024-10-22 16:30:09 +0530

    Added files

--- week-6-class/src/App.jsx
+++ week-6-class/src/App.jsx
@@ -1,26 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-  //*
+//*
 //* Create a react app that has a
 //* 1. Header component that takes a title as a prop and renders it inside a div
 //* 2. The top level App component renders 2 Headers
 
-// function App() {
-//   return (
-//     <div>
-//       <Header title="my name is parijat" />
-//       <Header title="My name is bhattacharjee" />
-//     </div>
-//   )
-// }
+function App() {
+  return (
+    <>
+      <Header title="my name is parijat" />
+      <Header title="My name is bhattacharjee" />
+    </>
+  );
+}
 
-// function Header({title}) {
-//   return <div>
-//     {title}
-//   </div>
-// }
+function Header({ title }) {
+  return <div>{title}</div>;
+}
 
-export default App
+export default App;
