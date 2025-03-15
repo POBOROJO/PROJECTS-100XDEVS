19b48d8c08c1b3310030d65a11ecc4951e8188af
POBOROJO <parijatbhatt1@gmail.com>
2025-03-16 01:12:21 +0530

    Update user model schema with validation rules

--- week-8/paytm/backend/models/User.js
+++ week-8/paytm/backend/models/User.js
@@ -1,11 +1,34 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-  username: String,
-  firstName: String,
-  lastName: String,
-  email: String,
-  password: String,
+  username:{
+    type:String,
+    required:true,
+    unique:true,
+    trime:true,
+    lowercase:true,
+    minLenght:3,
+    maxLength:20
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  firstName:{
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = model("User", userSchema);
